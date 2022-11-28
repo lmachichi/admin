2c26ffb3c27c4e7ce95287c40be14bff21b195c2
h m <s.lmachichi@gmail.com>
2022-11-28 19:56:07 +0100

    commit

---
+++ docs.php
@@ -0,0 +1,20 @@
+<?php
+        function doc(){
+        header("Content-Type: application/json");
+        session_start();
+        $conn = mysqli_connect("localhost", "root", "", "api");
+        if(mysqli_connect_error()){
+            die("fail To Connect to MySql" . mysqli_connect_error());
+        }
+        
+        $query = "SELECT * FROM `docs` ORDER BY `docs`.`docCommand` DESC";
+        $results = $conn->query($query);
+
+        $output = [];
+        while($item = $results->fetch_assoc()){
+            $output[] = $item;
+        }
+        //print_r($output);
+        echo json_encode($output);
+    }   
+    doc();
